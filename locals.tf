a4dbcfb6092c850d02f49a27c28589473fdb23e5
Charles Green <cg@simplycubed.com>
2022-01-18 20:41:24 +0900

    Auto format

--- locals.tf
+++ locals.tf
@@ -3,12 +3,12 @@ locals {
 
   # Fill in defaults for environment variables.
   env = toset([
-    for e in var.env: {
-      key = e.key
+    for e in var.env : {
+      key   = e.key
       value = e.value
       secret = {
-        name = e.secret
-        alias = e.secret != null ? lookup(local.secrets_to_aliases, e.secret, null) : null
+        name    = e.secret
+        alias   = e.secret != null ? lookup(local.secrets_to_aliases, e.secret, null) : null
         version = coalesce(e.version, "latest")
       }
     }
@@ -16,17 +16,17 @@ locals {
 
   // Fill in defaults for volumes.
   volumes = toset([
-    for vol in var.volumes: {
+    for vol in var.volumes : {
       path = vol.path
       name = "volume-${substr(sha1(jsonencode(vol)), 0, 4)}"
       secret = {
-        name = vol.secret
+        name  = vol.secret
         alias = lookup(local.secrets_to_aliases, vol.secret, null)
       }
       items = [
-        for filename, version in coalesce(vol.versions, { latest = "latest" }): {
+        for filename, version in coalesce(vol.versions, { latest = "latest" }) : {
           filename = filename,
-          version = version
+          version  = version
         }
       ]
     }
@@ -35,10 +35,10 @@ locals {
   // Map secrets in other projects to aliases. This allows for easy lookup when building up `local.env` and `local.volumes`.
   secrets_to_aliases = {
     for secret in distinct(concat(
-        [for env in var.env: env.secret if env.secret != null],
-        [for vol in var.volumes: vol.secret if vol.secret != null]
-      )):
-      secret => "secret-${substr(sha1(secret), 0, 4)}"
+      [for env in var.env : env.secret if env.secret != null],
+      [for vol in var.volumes : vol.secret if vol.secret != null]
+    )) :
+    secret => "secret-${substr(sha1(secret), 0, 4)}"
     if length(split("/", secret)) > 1
   }
   // It seems like a BETA launch stage is still okay for functionality in PREVIEW.
@@ -49,6 +49,6 @@ locals {
   // Ensure backwards-compatibility for the change in VPC access variables.
   vpc_access = {
     connector = coalesce(var.vpc_access.connector, var.vpc_connector_name, "-") == "-" ? null : coalesce(var.vpc_access.connector, var.vpc_connector_name)
-    egress = coalesce(var.vpc_access.egress, var.vpc_access_egress, "private-ranges-only")
+    egress    = coalesce(var.vpc_access.egress, var.vpc_access_egress, "private-ranges-only")
   }
 }
--- main.tf
+++ main.tf
@@ -1,23 +1,22 @@
-
-resource google_cloud_run_service default {
+resource "google_cloud_run_service" "default" {
   provider = google-beta
 
-  name = var.name
-  location = var.location
+  name                       = var.name
+  location                   = var.location
   autogenerate_revision_name = true
-  project = local.project_id
+  project                    = local.project_id
 
   metadata {
     namespace = local.project_id
-    labels = var.labels
+    labels    = var.labels
     annotations = merge(
       {
-        "run.googleapis.com/ingress" = var.ingress
+        "run.googleapis.com/ingress"     = var.ingress
         "run.googleapis.com/client-name" = "terraform"
-        "client.knative.dev/user-image" = var.image
+        "client.knative.dev/user-image"  = var.image
       },
       length(local.secrets_to_aliases) < 1 ? {} : {
-        "run.googleapis.com/secrets" = join(",", [for secret, alias in local.secrets_to_aliases: "${alias}:${secret}"])
+        "run.googleapis.com/secrets" = join(",", [for secret, alias in local.secrets_to_aliases : "${alias}:${secret}"])
       }
     )
   }
@@ -34,62 +33,62 @@ resource google_cloud_run_service default {
   template {
     spec {
       container_concurrency = var.concurrency
-      timeout_seconds = var.timeout
-      service_account_name = var.service_account_email
+      timeout_seconds       = var.timeout
+      service_account_name  = var.service_account_email
 
       containers {
-        image = var.image
+        image   = var.image
         command = var.entrypoint
-        args = var.args
+        args    = var.args
 
         ports {
-          name = var.http2 ? "h2c" : "http1"
+          name           = var.http2 ? "h2c" : "http1"
           container_port = var.port
         }
 
         resources {
           limits = {
-            cpu = "${var.cpus * 1000}m"
+            cpu    = "${var.cpus * 1000}m"
             memory = "${var.memory}Mi"
           }
         }
 
         # Populate straight environment variables.
-        dynamic env {
-          for_each = [for e in local.env: e if e.value != null]
+        dynamic "env" {
+          for_each = [for e in local.env : e if e.value != null]
 
           content {
-            name = env.value.key
+            name  = env.value.key
             value = env.value.value
           }
         }
 
         # Populate environment variables from secrets.
-        dynamic env {
-          for_each = [for e in local.env: e if e.secret.name != null]
+        dynamic "env" {
+          for_each = [for e in local.env : e if e.secret.name != null]
 
           content {
             name = env.value.key
             value_from {
               secret_key_ref {
                 name = coalesce(env.value.secret.alias, env.value.secret.name)
-                key = env.value.secret.version
+                key  = env.value.secret.version
               }
             }
           }
         }
 
-        dynamic volume_mounts {
+        dynamic "volume_mounts" {
           for_each = local.volumes
 
           content {
-            name = volume_mounts.value.name
+            name       = volume_mounts.value.name
             mount_path = volume_mounts.value.path
           }
         }
       }
 
-      dynamic volumes {
+      dynamic "volumes" {
         for_each = local.volumes
 
         content {
@@ -98,11 +97,11 @@ resource google_cloud_run_service default {
           secret {
             secret_name = coalesce(volumes.value.secret.alias, volumes.value.secret.name)
 
-            dynamic items {
+            dynamic "items" {
               for_each = volumes.value.items
 
               content {
-                key = items.value.version
+                key  = items.value.version
                 path = items.value.filename
               }
             }
@@ -115,45 +114,45 @@ resource google_cloud_run_service default {
       labels = var.labels
       annotations = merge(
         {
-          "run.googleapis.com/cpu-throttling" = var.cpu_throttling
+          "run.googleapis.com/cpu-throttling"     = var.cpu_throttling
           "run.googleapis.com/cloudsql-instances" = join(",", var.cloudsql_connections)
-          "autoscaling.knative.dev/maxScale" = var.max_instances
-          "autoscaling.knative.dev/minScale" = var.min_instances
+          "autoscaling.knative.dev/maxScale"      = var.max_instances
+          "autoscaling.knative.dev/minScale"      = var.min_instances
         },
         local.vpc_access.connector == null ? {} : {
           "run.googleapis.com/vpc-access-connector" = local.vpc_access.connector
-          "run.googleapis.com/vpc-access-egress" = local.vpc_access.egress
+          "run.googleapis.com/vpc-access-egress"    = local.vpc_access.egress
         },
         length(local.secrets_to_aliases) < 1 ? {} : {
-          "run.googleapis.com/secrets" = join(",", [for secret, alias in local.secrets_to_aliases: "${alias}:${secret}"])
+          "run.googleapis.com/secrets" = join(",", [for secret, alias in local.secrets_to_aliases : "${alias}:${secret}"])
         },
       )
     }
   }
 
   traffic {
-    percent = 100
+    percent         = 100
     latest_revision = var.revision == null
-    revision_name = var.revision != null ? "${var.name}-${var.revision}" : null
+    revision_name   = var.revision != null ? "${var.name}-${var.revision}" : null
   }
 }
 
 
-resource google_cloud_run_service_iam_member public_access {
-  count = var.allow_public_access ? 1 : 0
-  service = google_cloud_run_service.default.name
+resource "google_cloud_run_service_iam_member" "public_access" {
+  count    = var.allow_public_access ? 1 : 0
+  service  = google_cloud_run_service.default.name
   location = google_cloud_run_service.default.location
-  project = google_cloud_run_service.default.project
-  role = "roles/run.invoker"
-  member = "allUsers"
+  project  = google_cloud_run_service.default.project
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
 
-resource google_cloud_run_domain_mapping domains {
+resource "google_cloud_run_domain_mapping" "domains" {
   for_each = var.map_domains
 
   location = google_cloud_run_service.default.location
-  project = google_cloud_run_service.default.project
-  name = each.value
+  project  = google_cloud_run_service.default.project
+  name     = each.value
 
   metadata {
     namespace = google_cloud_run_service.default.project
